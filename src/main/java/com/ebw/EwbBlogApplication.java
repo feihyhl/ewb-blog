33a866927bb24180821a8ad1d879c8ff6ce2e85b
hyhl <feihyhl@163.com>
2020-04-22 11:36:07 +0800

    初始化

---
+++ src/main/java/com/ebw/EwbBlogApplication.java
@@ -0,0 +1,13 @@
+package com.ebw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwbBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwbBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebw/EwbBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwbBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
